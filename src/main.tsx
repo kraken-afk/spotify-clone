b15f710ab310879b5806c9bab0b1063553194d28
Romeo Noveanre <noveanrer@gmail.com>
2023-07-08 09:09:44 +0700

    add dev mode -1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { devMode } from "./utils/dev-mode";
 
 const root = createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+devMode();
 
 root.render(
   <StrictMode>
