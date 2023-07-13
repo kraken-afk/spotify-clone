0db68f0e2fe8c9a0e137266982d304c220bbc784
Romeo Noveanre <noveanrer@gmail.com>
2023-07-13 17:58:25 +0700

    minor

--- src/hooks/useFetchSpotify.ts
+++ src/hooks/useFetchSpotify.ts
@@ -1,11 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 
-export default function useFetchSpotify<T = unknown>(url: string, accessToken: string, metadata?: RequestInit) {
-  return useQuery({
-    queryKey: [new URL(url).pathname],
-    cacheTime: 3600,
-    queryFn: async () => {
-      // TODO: undone
-    },
-  });
-}
+// export default function useFetchSpotify<T = unknown>(url: string, accessToken: string, metadata?: RequestInit) {
+//   return useQuery({
+//     queryKey: [new URL(url).pathname],
+//     cacheTime: 3600,
+//     queryFn: async () => {
+//       // TODO: undone
+//     },
+//   });
+// }
--- src/hooks/useGet.ts
+++ src/hooks/useGet.ts
@@ -1,10 +1,10 @@
-import { type UseQueryResult, useQuery } from "@tanstack/react-query";
+// import { type UseQueryResult, useQuery } from "@tanstack/react-query";
 
-export default function useGet<T>(url: string, headers?: HeadersInit): UseQueryResult<T> {
-  return useQuery({
-    queryFn: async () => {
-      // // TODO: undone
-    },
-    refetchOnWindowFocus: false,
-  })
-}
+// export default function useGet<T>(url: string, headers?: HeadersInit): UseQueryResult<T> {
+//   return useQuery({
+//     queryFn: async () => {
+//       // // TODO: undone
+//     },
+//     refetchOnWindowFocus: false,
+//   })
+// }
