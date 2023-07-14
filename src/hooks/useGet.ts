5d9d3609202fe8d930deacee347a063f5c4c5dd8
Romeo Noveanre <noveanrer@gmail.com>
2023-07-14 17:38:51 +0700

    adding useGet

--- src/hooks/useGet.ts
+++ src/hooks/useGet.ts
@@ -1,10 +1,14 @@
-// import { type UseQueryResult, useQuery } from "@tanstack/react-query";
+import { type UseQueryResult, useQuery } from "@tanstack/react-query";
 
-// export default function useGet<T>(url: string, headers?: HeadersInit): UseQueryResult<T> {
-//   return useQuery({
-//     queryFn: async () => {
-//       // // TODO: undone
-//     },
-//     refetchOnWindowFocus: false,
-//   })
-// }
+export default function useGet<T>(url: string, metadata?: RequestInit): UseQueryResult<T> {
+  return useQuery({
+    cacheTime: 1800,
+    queryFn: async () => {
+      const response = await fetch(url, metadata);
+      const data = await response.json();
+
+      return data;
+    },
+    refetchOnWindowFocus: false,
+  })
+}
