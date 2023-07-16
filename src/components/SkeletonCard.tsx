ea82e4325fcd6dcc7bd19eee4675946b5ce04f8e
Romeo Noveanre <noveanrer@gmail.com>
2023-07-16 23:03:33 +0700

    add skeleton loading

---
+++ src/components/SkeletonCard.tsx
@@ -0,0 +1,15 @@
+import { type ReactElement } from "react";
+import "~/styles/card.scss";
+
+export default function SkeletonCard(): ReactElement {
+  return (
+    <div
+      className={`w-48 h-64 p-4 rounded-md bg-black-expose shadow-md hover:bg-neutral-800 transition-all duration-300 cursor-pointer relative card`}
+    >
+      <picture className="block h-[158px] w-full object-center rounded-sm overflow-hidden shadow-sm">
+        <div className="bg-neutral-700 w-auto h-full animate-pulse rounded-2xl"></div>
+      </picture>
+      <div className="bg-neutral-700 w-auto h-10 mt-4 animate-pulse rounded-xl"></div>
+    </div>
+  );
+}
--- src/pages/home/YourPlaylists.tsx
+++ src/pages/home/YourPlaylists.tsx
@@ -7,18 +7,19 @@ import CredentialContext from "~/context/CredentialContext";
 import "swiper/css";
 import "swiper/css/scrollbar";
 import "~/styles/utils.scss";
+import SkeletonCard from "~/components/SkeletonCard";
 
 export default function YourPlaylists(): ReactElement {
   const containerRef = useRef<HTMLElement>(null);
   const cardRef = useRef<HTMLDivElement>(null);
   const [slidesCount, setSlidesCount] = useState<number>(0);
   const token = useContext(CredentialContext) as Credential;
-  const { data } = useFetchSpotify("https://api.spotify.com/v1/me/playlists?limit=10", token, { method: "GET" });
+  const { data, isLoading } = useFetchSpotify("https://api.spotify.com/v1/me/playlists?limit=10", token, { method: "GET" });
 
   console.log(data)
 
   useLayoutEffect(() => {
-    if (slidesCount) return;
+    if (slidesCount || isLoading) return;
 
     const containerX = containerRef.current?.clientWidth as number;
     const cardX = cardRef.current?.clientWidth as number;
@@ -32,22 +33,28 @@ export default function YourPlaylists(): ReactElement {
   return (
     <section ref={containerRef} className="my-4 overflow-hidden">
       <h2 className="sub-title mb-4">Your playlist</h2>
-      <Swiper
-        className="py-4 cursor-grab active:cursor-grabbing"
-        spaceBetween={16}
-        slidesPerView={slidesCount}
-        scrollbar={{ draggable: true }}
-        modules={[Scrollbar, Virtual]}
-      >
-        <SwiperSlide>
-          <Card
-            ref={cardRef}
-            coverImage="https://picsum.photos/300"
-            title="Indie Gaming"
-            description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero quisquam veritatis provident aperiam cupiditate officiis. Vitae error iure repellat officiis debitis, facilis cumque enim consequatur! Accusamus expedita repellendus tenetur dolorem."
-          />
-        </SwiperSlide>
-      </Swiper>
+      {isLoading ? (
+        <div className="flex gap-4">
+          <SkeletonCard /> <SkeletonCard /> <SkeletonCard />
+        </div>
+      ) : (
+        <Swiper
+          className="py-4 cursor-grab active:cursor-grabbing"
+          spaceBetween={16}
+          slidesPerView={slidesCount}
+          scrollbar={{ draggable: true }}
+          modules={[Scrollbar, Virtual]}
+        >
+          <SwiperSlide>
+            <Card
+              ref={cardRef}
+              coverImage="https://picsum.photos/300"
+              title="Indie Gaming"
+              description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero quisquam veritatis provident aperiam cupiditate officiis. Vitae error iure repellat officiis debitis, facilis cumque enim consequatur! Accusamus expedita repellendus tenetur dolorem."
+            />
+          </SwiperSlide>
+        </Swiper>
+      )}
     </section>
   );
 }
