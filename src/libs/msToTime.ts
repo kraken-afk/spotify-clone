64803333965651b87144e00c1a7de6f796719c09
Romeo Noveanre <noveanrer@gmail.com>
2023-07-20 12:45:31 +0700

    add msToTime function

---
+++ src/libs/msToTime.ts
@@ -0,0 +1,34 @@
+export default function msToTime(ms: number): string {
+  if (ms <= 0) throw new TypeError("ms cannot be equal or below zero!");
+
+  let second: number = +(ms / 1000).toFixed();
+  let minute: number = 0;
+  let hour: number = 0;
+
+  while (second > 60) {
+    second -= 60;
+    minute++;
+  }
+
+  while (minute > 60) {
+    minute -= 60;
+    hour++;
+  }
+
+  if (second) {
+    second = 0;
+    minute++;
+  }
+
+  let result: string = "";
+
+  if (hour)
+    result = result.concat(`${hour} hr${hour === 1 ? "" : "s"} `);
+
+  if (minute)
+    result = result.concat(`${minute} min${minute === 1 ? "" : "s"} `);
+
+  console.log(result);
+
+  return result;
+}
