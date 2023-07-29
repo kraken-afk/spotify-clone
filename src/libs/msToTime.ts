eedfbc006ac2f0f8528cb11c3fe4e9a48b6ebcb6
Romeo Noveanre <noveanrer@gmail.com>
2023-07-29 16:25:17 +0700

    feat(msToTime):  duration calc from ms

--- src/libs/msToTime.ts
+++ src/libs/msToTime.ts
@@ -1,30 +1,39 @@
-export default function msToTime(ms: number): string {
+export enum TimeConverterEnum {
+  DURATION = "duration",
+  ESTIMATE = "estimate",
+}
+
+export default function msToTime(ms: number, type?: TimeConverterEnum): string {
   if (ms <= 0) throw new TypeError("ms cannot be equal or below zero!");
 
   let second: number = +(ms / 1000).toFixed();
   let minute: number = 0;
   let hour: number = 0;
 
-  while (second > 60) {
+  while (second >= 60) {
     second -= 60;
     minute++;
   }
 
-  while (minute > 60) {
+  while (minute >= 60) {
     minute -= 60;
     hour++;
   }
 
-  if (second) {
-    second = 0;
-    minute++;
-  }
-
   let result: string = "";
 
-  if (hour) result = result.concat(`${hour} hr${hour === 1 ? "" : "s"} `);
+  if (type === TimeConverterEnum.DURATION) {
+    if (hour > 0) {
+      result = `${hour.toString()}:${minute.toString().padStart(2, "0")}:${second
+        .toString()
+        .padStart(2, "0")}`;
+    } else {
+      result = `${minute.toString()}:${second.toString().padStart(2, "0")}`;
+    }
+  } else {
+    if (hour) result = result.concat(`${hour} hr${hour === 1 ? "" : "s"} `);
+    if (minute) result = result.concat(`${minute} min${minute === 1 ? "" : "s"} `);
+  }
 
-  if (minute) result = result.concat(`${minute} min${minute === 1 ? "" : "s"} `);
-
-  return result;
+  return result.trim();
 }
