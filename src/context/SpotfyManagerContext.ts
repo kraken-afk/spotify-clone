73643cc1770de4729b73f867b04f7f0f7248cda4
Romeo Noveanre <noveanrer@gmail.com>
2023-08-03 13:46:21 +0700

    delete unused modules

--- src/context/SpotfyManagerContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-import SpotifyDatasManager from "~/libs/SpotifyDatasManager";
-
-const spotifyManager = new SpotifyDatasManager();
-const SpotifyManagerContext = createContext<SpotifyDatasManager>(spotifyManager);
-
-export default SpotifyManagerContext;
