fb74dedcdfd26b39c7b8db0a9a0a3f59e58d8267
Romeo Noveanre <noveanrer@gmail.com>
2023-08-28 12:27:26 +0700

    open for public

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Loader from "./components/popups/Loader";
 
 const queryClient = new QueryClient();
 const root = createRoot(document.getElementById("root") as HTMLElement);
